d0cb3f1ab0e3eeb479bc382f777c85683b02b1e9
ilya-hontarau <ilyah@spacelift.io>
2025-09-01 18:11:05 +0200

    feat: add eks outputs

--- outputs.tf
+++ outputs.tf
@@ -92,3 +92,14 @@ output "eks_cluster_name" {
   value       = local.cluster_name
   description = "Name of the EKS cluster."
 }
+
+output "eks_cluster_endpoint" {
+  value       = module.eks.cluster_endpoint
+  description = "Endpoint of the EKS cluster."
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
