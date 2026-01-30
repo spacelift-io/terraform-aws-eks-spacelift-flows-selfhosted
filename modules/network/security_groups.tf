aba1e5bcc83684a6cc4442f7907bdab861d0b2b0
ilya-hontarau <ilyah@spacelift.io>
2026-01-30 16:00:35 +0100

    fix: drop db egress rule

--- modules/network/security_groups.tf
+++ modules/network/security_groups.tf
@@ -4,13 +4,3 @@ resource "aws_security_group" "database" {
   description = "Security group for Flows database"
   vpc_id      = module.vpc.vpc_id
 }
-
-# Allow egress from database (for updates, etc.)
-resource "aws_security_group_rule" "database_egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.database.id
-}
